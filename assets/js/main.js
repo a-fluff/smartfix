159cd3694ecb01b033f8b4a5487bf5119bce8da0
Anna <80855277+a-fluff@users.noreply.github.com>
2021-07-05 18:24:47 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,16 @@
-// var menu = document.querySelector('.menu');
-// var open = false;
+var menu = document.querySelector('.menu');
+var open = false;
 
 
-// document.querySelector('.burger-box').onclick = function(){
-//     if(open){
-//         menu.classList.remove('menu--open');
-//     }else{
-//         menu.classList.add('menu--open');
-//     }
+document.querySelector('.burger').onclick = function(){
+    if(open){
+        menu.classList.remove('menu--open');
+    }else{
+        menu.classList.add('menu--open');
+    }
 
-//     open = !open;
-// };
-
-// var burgerBox = document.querySelector('.burger-box--closed');
-// var menu = document.getElementsByClassName('.menu');
-
-// burgerBox.onclick = function () {
-//     menu.classList.toggle('open');
-// }
-
-
-// $(document).ready(function() {
-//     $('.burger-box--closed').click(function(event) {
-//         $('.burger-box, .menu').toggleClass('active');
-//     });
-// });
+    open = !open;
+};
 
 window.onload = function () {
     document.body.classList.add('loaded_hiding');
@@ -34,40 +20,6 @@ window.onload = function () {
     },500);
 }
 
-// $(document).ready(function() {
-//     let position = 0;
-//     const slidesToShow = 3;
-//     const slidesToScroll = 2;
-//     const container = $('.slider__container');
-//     const track = $('.slider__list');
-//     const item = $('.slider__item');
-//     const btnPrev = $('.previous');
-//     const btnNext = $('.next');
-//     const itemWidth = container.wisth() / slidesToShow;
-
-//     item.each(function (index, item) {
-//         $(item).css({
-//             minWidth: itemWidth,
-//         });
-//     });
-// });
-
-
-
-// $(document).ready(function () {
-// 	$(".icons").slick({
-// 		//Basic settings
-// 		autoplay: true,
-// 		autoplaySpeed: 400000,
-// 		arrows: true,
-// 		swipeToSlide: true,
-// 		touchMove: true,
-// 		ltr: true,
-// 		infinite: true,
-// 		variableWidth: true,
-// 		dots: false,
-// 		pauseOnHover: false
-// 	});
-// });
-
-
+document.querySelector('.burger').addEventListener('click', function(){
+  document.querySelector('.burger span').classList.toggle('active');
+})
