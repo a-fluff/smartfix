a16978c85c909662f41368b9619c6d3919d090ef
Anna <80855277+a-fluff@users.noreply.github.com>
2021-07-06 16:55:39 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
+
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    },500);
+}
+
 var menu = document.querySelector('.menu');
 var open = false;
-
 
 document.querySelector('.burger').onclick = function(){
     if(open){
@@ -12,19 +20,13 @@ document.querySelector('.burger').onclick = function(){
     open = !open;
 };
 
-window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
-    },500);
-}
+
 
 document.querySelector('.burger').addEventListener('click', function(){
   document.querySelector('.burger span').classList.toggle('active');
 })
 
 
+
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
